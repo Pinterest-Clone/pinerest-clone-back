50a8be7cadaed3d1a5d614d7c471aa726a12607b
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-22 11:33:05 +0900

    feat: 회원정보 수정 구현중

---
+++ src/main/java/com/sparta/pinterest_clone/dto/UpdateProfileResponseDto.java
@@ -0,0 +1,14 @@
+package com.sparta.pinterest_clone.dto;
+
+public class UpdateProfileResponseDto {
+    private String firstname;
+
+    private String lastname;
+
+    private String introduce;
+
+    private String url;
+    private String username;
+
+
+}
