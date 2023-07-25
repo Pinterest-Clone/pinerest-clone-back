e44125c1da0d8b0df52936e07edf94ee9b941a04
userkim12 <potomq444@naver.com>
2023-07-25 09:47:06 +0900

    Feat: security cors 설정 추가

--- src/main/java/com/sparta/pinterest_clone/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/pinterest_clone/config/WebSecurityConfig.java
@@ -17,6 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -72,5 +77,15 @@ public class WebSecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/sparta/pinterest_clone/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.sparta.pinterest_clone.repository;
-//
-//import com.sparta.pinterest_clone.entity.Comment;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CommentRepository extends JpaRepository<Comment, Long> {
-//}
