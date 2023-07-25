1295e9889a6d42ae78ff3bb4d95313aa5a0cccec
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-07-25 15:48:19 +0900

    feat: 댓글 좋아요 기능 추가

---
+++ src/main/java/com/sparta/pinterest_clone/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.pinterest_clone.comment.repository;
+
+import com.sparta.pinterest_clone.comment.entity.Comment;
+import com.sparta.pinterest_clone.comment.entity.CommentLike;
+import com.sparta.pinterest_clone.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+    CommentLike findByCommentAndUser(Comment comment, User user);
+}
